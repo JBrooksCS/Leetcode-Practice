61e9a06624374acd0616b16746494405b852eae0
JBrooksCS <jasonbrooksbio@gmail.com>
2019-10-22 17:55:14 -0500

    Added the Valid Braces exercise

--- codewars.py
+++ codewars.py
@@ -211,3 +211,66 @@ def make_readable(seconds):
     # print((str(f'{hours:02n}') + ":" + str(f'{minutes:02n}') + ":" + str(f'{seconds:02n}')))
     
     return ((str(f'{hours:02n}') + ":" + str(f'{minutes:02n}') + ":" + str(f'{seconds:02n}')))
+
+"""
+Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+"""
+def validBraces(string):
+  
+  closingStack = []
+
+  for c in string:
+      #                 PARENS
+      if c == "(":
+          closingStack.insert(0, ")")
+          
+      elif c == ")":
+          if len(closingStack) == 0:
+              return False
+              
+          elif closingStack[0] == ")":
+              closingStack.pop(0)
+          else:
+              return False
+      #                 BRACKETS
+      elif c == "[":
+          closingStack.insert(0, "]")
+          
+      elif c == "]":
+          if len(closingStack) == 0:
+                  return False
+          elif closingStack[0] == "]":
+              closingStack.pop(0)
+          else:
+              return False
+      #                 BRACES
+      elif c == "{":
+          closingStack.insert(0, "}")
+          
+      elif c == "}":
+          if len(closingStack) == 0:
+              return False
+          elif closingStack[0] == "}":
+              closingStack.pop(0)
+          else:
+              return False
+  if len(closingStack) == 0 :
+      return True
+  else :
+      return False
+      
+      
