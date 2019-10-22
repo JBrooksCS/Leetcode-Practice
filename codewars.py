c4a545ff1a0e7fa589d830cf8df1136c4252d4bd
JBrooksCS <jasonbrooksbio@gmail.com>
2019-10-22 17:14:34 -0500

    added Human Readable Time exercise

--- codewars.py
+++ codewars.py
@@ -158,7 +158,7 @@ def create_spiral(n):
     while counter <= n*n:
         ######RIGHT
         for i in range (startCol, endCol + 1):
-            arr[startCol][i] = counter
+            arr[startRow][i] = counter
             counter += 1
         startRow += 1
         
@@ -181,3 +181,33 @@ def create_spiral(n):
         startCol += 1
     
     return arr
+
+"""
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+
+Test.assert_equals(make_readable(0), "00:00:00")
+Test.assert_equals(make_readable(5), "00:00:05")
+Test.assert_equals(make_readable(60), "00:01:00")
+Test.assert_equals(make_readable(86399), "23:59:59")
+Test.assert_equals(make_readable(359999), "99:59:59")
+"""
+
+def make_readable(seconds):
+    # Do something
+    minutes = (seconds // 60) % 60
+    hours = (seconds // (60*60)) 
+    seconds = seconds - (hours * 60 * 60) - (minutes * 60)
+    
+    # print("HOURS - ", hours)
+    # print("MINUTES - ", minutes)
+    # print("MINUTES - ", seconds)
+    # print((str(f'{hours:02n}') + ":" + str(f'{minutes:02n}') + ":" + str(f'{seconds:02n}')))
+    
+    return ((str(f'{hours:02n}') + ":" + str(f'{minutes:02n}') + ":" + str(f'{seconds:02n}')))
