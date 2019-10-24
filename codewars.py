8811408c3f9e15ad23e8f31a27f1587392731154
JBrooksCS <jasonbrooksbio@gmail.com>
2019-10-24 13:10:34 -0500

    added digital root exercise

--- codewars.py
+++ codewars.py
@@ -274,3 +274,38 @@ def validBraces(string):
       return False
       
       
+"""
+In this kata, you must create a digital root function.
+
+A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+Here's how it works:
+
+digital_root(16)
+=> 1 + 6
+=> 7
+
+digital_root(942)
+=> 9 + 4 + 2
+=> 15 ...
+=> 1 + 5
+=> 6
+"""
+def digital_root(n):
+    # ...
+    
+    listOfNum = intToList(n)
+    
+    i = 0
+    
+    while len(listOfNum) > 1:
+        for n in listOfNum:
+            i += int(n)
+        listOfNum = intToList(i)
+        i = 0
+            
+    return (int(listOfNum[0]))
+
+    
+def intToList(input):
+    return list(str(input))
