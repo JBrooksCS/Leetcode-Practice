77e1fbe12cd25710812263ea45c4992ed3b3ad85
JBrooksCS <jasonbrooksbio@gmail.com>
2019-10-25 12:31:10 -0500

    Added Vasya Clerk exercise

--- codewars.py
+++ codewars.py
@@ -309,3 +309,59 @@ def digital_root(n):
     
 def intToList(input):
     return list(str(input))
+
+"""
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+
+Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+
+Examples:
+tickets([25, 25, 50]) # => YES 
+tickets([25, 100]) # => NO. Vasya will not have enough money to give change to 100 dollars
+tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+
+"""
+
+def tickets(people):
+
+    numTwentyFives = 0
+    numFifties = 0
+    numHundreds = 0
+    
+    for person in people:
+    
+        if person == 25:
+            numTwentyFives += 1
+            
+        elif person == 50:
+            if numTwentyFives > 0:
+                numFifties += 1
+                numTwentyFives -= 1
+            else: 
+                return 'NO'
+                
+        elif person == 100:
+        
+            if numFifties > 0 and numTwentyFives > 0:
+                numHundreds += 1
+                numFifties -= 1
+                numTwentyFives -= 1
+                
+            elif numTwentyFives >= 3:
+                numHundreds += 1
+                numTwentyFives -= 3
+                
+            else: 
+                return 'NO'
+                
+    return 'YES'
+                
+                
+                
+
+
+
